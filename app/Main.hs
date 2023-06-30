6281244c53397615e4c8e527134b8f83f165b6ef
Amogh Rathore <amoghdroid09@gmail.com>
2023-06-29 18:29:31 -0700

    refactor main

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import qualified Advent.Day1 as Day1
 import qualified Advent.Day2 as Day2
+import Data.ByteString.Lazy.Char8 (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as B
 import System.Environment (getArgs)
 
@@ -12,9 +13,11 @@ main = do
         "real" -> "inputs/" <> day <> ".txt"
         "example" -> "inputs/" <> day <> ".example.txt"
         _ -> error "bad input type"
-  input <- B.readFile inputFile
-  let res = case part of
-        "1" -> Day2.solve input
-        "2" -> Day2.solve2 input
-        _ -> error "bad part"
-  B.putStrLn res
+  B.readFile inputFile >>= B.putStrLn . solve day part
+
+solve :: String -> String -> ByteString -> ByteString
+solve "day1" "part1" = Day1.solve
+solve "day1" "part2" = Day1.solve2
+solve "day2" "part1" = Day2.solve
+solve "day2" "part2" = Day2.solve2
+solve _ _ = error "bad input day and part"
