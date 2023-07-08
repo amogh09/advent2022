a8b9e7ab1adbe2ab0b10715a388a77dc1f87e476
Amogh Rathore <amoghdroid09@gmail.com>
2023-07-08 10:27:09 -0700

    Optimized Day8 part 1

--- src/Advent/Day8.hs
+++ src/Advent/Day8.hs
@@ -3,102 +3,73 @@ module Advent.Day8 (solve1) where
 import Advent.Util (bshow)
 import qualified Data.ByteString.Lazy.Char8 as B
 import Data.Char (digitToInt)
-import Data.List (transpose)
 import GHC.Arr (Array)
 import qualified GHC.Arr as A
 import GHC.List (foldl')
 
+{- ORMOLU_DISABLE -}
 type Matrix a = Array (Int, Int) a
-
 type Grid = Matrix Int
+type Coordinates = (Int, Int)
+{- ORMOLU_ENABLE -}
 
 solve1 :: B.ByteString -> B.ByteString
-solve1 =
-  bshow
-    . sum
-    . fmap fromEnum
-    . A.elems
-    . visibleGrid
-    . fmap (fmap digitToInt . B.unpack)
-    . B.lines
+solve1 = bshow . sum . fmap fromEnum . A.elems . visibleGrid . parseGrid
 
 -- >>> parseGrid "0123\n2345"
 -- array ((0,0),(1,3)) [((0,0),0),((0,1),1),((0,2),2),((0,3),3),
 --                      ((1,0),2),((1,1),3),((1,2),4),((1,3),5)]
--- parseGrid :: B.ByteString -> Grid
--- parseGrid s = do
---   let ls = B.lines s
---       (m, n) = (length ls, fromInteger . toInteger . B.length $ head ls)
---       (lo, hi) = ((0, 0), (m - 1, n - 1))
---   A.listArray (lo, hi) . mconcat . fmap (fmap digitToInt . B.unpack) $ ls
+parseGrid :: B.ByteString -> Grid
+parseGrid s = do
+  let ls = B.lines s
+      (m, n) = (length ls, fromInteger . toInteger . B.length $ head ls)
+      (lo, hi) = ((0, 0), (m - 1, n - 1))
+  A.listArray (lo, hi) . mconcat . fmap (fmap digitToInt . B.unpack) $ ls
 
--- visibleLeft :: Grid -> [(Int, Int)] -> [(Int, Int)]
--- visibleLeft grid = snd . foldl' f (minBound, [])
---   where
---     f :: (Int, [(Int, Int)]) -> (Int, Int) -> (Int, [(Int, Int)])
---     f = undefined
-
--- f (currentMax, indices) (i, x)
---   | x > currentMax = (x, i : indices)
---   | otherwise = (currentMax, indices)
-
--- | Given a grid of tree heights, returns a grid of Booleans (one for each height) where
--- True means that the tree is visible from at least one direction, and False otherwise.
+-- | Converts a grid of tree heights to a grid of booleans where True means the tree
+-- is visible and False means that the tree is not visible.
 --
--- >>> visibleGrid [[3,0,3,7,3], [2,5,5,1,2], [6,5,3,3,2]]
--- array ((0,0),(2,4)) [((0,0),True),((0,1),True),((0,2),True),((0,3),True),((0,4),True),
---                      ((1,0),True),((1,1),True),((1,2),True),((1,3),False),((1,4),True),
---                      ((2,0),True),((2,1),True),((2,2),True),((2,3),True),((2,4),True)]
-visibleGrid :: [[Int]] -> Matrix Bool
-visibleGrid grid = visibleLeftOrRightGrid grid `merge` visibleLeftOrRightGrid (transpose grid)
+-- >>> visibleGrid (parseGrid "30373\n25512")
+-- array ((0,0),(1,4)) [((0,0),True),((0,1),True),((0,2),True),((0,3),True),((0,4),True),
+--                      ((1,0),True),((1,1),True),((1,2),True),((1,3),True),((1,4),True)]
+visibleGrid :: Matrix Int -> Matrix Bool
+visibleGrid g = A.accumArray (||) False (A.bounds g) . (`zip` repeat True) . visibleAny $ g
+
+-- | Returns a list of coordinates of trees visible from any direction in the grid.
+-- Duplicate coordinates are returned if a tree on a coordinate is visible from more than
+-- one direction.
+visibleAny :: Grid -> [Coordinates]
+visibleAny g =
+  mconcat (fmap (visibleLeftOrRight g) (rows g) <> fmap (visibleLeftOrRight g) (cols g))
+
+-- >>> visibleLeftOrRight (parseGrid "25512") (A.range ((0,0), (0, 4)))
+-- [(0,1),(0,0),(0,2),(0,4)]
+visibleLeftOrRight :: Grid -> [Coordinates] -> [Coordinates]
+visibleLeftOrRight g is = visibleLeft g is <> (visibleLeft g . reverse $ is)
+
+-- >>> visibleLeft (parseGrid "25512") (A.range ((0,0), (0, 4)))
+-- [(0,1),(0,0)]
+visibleLeft :: Grid -> [Coordinates] -> [Coordinates]
+visibleLeft grid = snd . foldl' f (minBound, [])
   where
-    merge :: Matrix Bool -> Matrix Bool -> Matrix Bool
-    merge m1 m2 =
-      let b = A.bounds m1
-       in A.array b [((i, j), m1 A.! (i, j) || m2 A.! (j, i)) | (i, j) <- A.range b]
+    f (currentMax, is) i
+      | grid A.! i > currentMax = (grid A.! i, i : is)
+      | otherwise = (currentMax, is)
 
--- | Given a grid of heights, returns a grid of booleans in which an element is True
--- if its corresponding index in the input grid is visible from either left or right, and
--- is False otherwise.
---
--- >>> visibleLeftOrRightGrid [[3, 0, 3, 7, 3], [2, 5, 5, 1, 2]]
--- array ((0,0),(1,4))
---       [((0,0),True),((0,1),False),((0,2),False),((0,3),True),((0,4),True),
---        ((1,0),True),((1,1),True),((1,2),True),((1,3),False),((1,4),True)]
-visibleLeftOrRightGrid :: [[Int]] -> Matrix Bool
-visibleLeftOrRightGrid grid =
-  A.accumArray (||) False ((0, 0), (m - 1, n - 1))
-    . mconcat
-    . fmap (\(i :: Int, row) -> (`zip` repeat True) . fmap (i,) . visibleLeftOrRight $ row)
-    . zip [0 ..]
-    $ grid
-  where
-    m = length grid
-    n = length $ head grid
+-- >>> rows (parseGrid "0123\n2345")
+-- [[(0,0),(0,1),(0,2),(0,3)],
+--  [(1,0),(1,1),(1,2),(1,3)]]
+rows :: Matrix a -> [[Coordinates]]
+rows g = do
+  let ((x1, y1), (x2, y2)) = A.bounds g
+  [[(i, j) | j <- A.range (y1, y2)] | i <- A.range (x1, x2)]
 
--- | Given a list of heights, returns a list of indices of trees that are visible
--- from either the left or the right side. Returns duplicate indices.
---
--- >>> visibleLeftOrRight [2, 5, 5, 1, 2]
--- [1,0,2,4]
---
--- >>> visibleLeftOrRight [1]
--- [0,0]
-visibleLeftOrRight :: [Int] -> [Int]
-visibleLeftOrRight hs = visibleLeft hs <> (fmap wrap . visibleLeft . reverse $ hs)
-  where
-    wrap i = n - 1 - i
-    n = length hs
-
--- | Given a list of heights, returns a list of indices of trees that are
--- visible from the left side.
---
--- >>> visibleLeft [2, 5, 5, 1, 2]
--- [1,0]
-visibleLeft :: [Int] -> [Int]
-visibleLeft = snd . foldl' f (minBound, []) . zip [0 ..]
-  where
-    f :: (Int, [Int]) -> (Int, Int) -> (Int, [Int])
-    f (currentMax, indices) (i, x)
-      | x > currentMax = (x, i : indices)
-      | otherwise = (currentMax, indices)
+-- >>> cols (parseGrid "0123\n2345")
+-- [[(0,0),(1,0)],
+--  [(0,1),(1,1)],
+--  [(0,2),(1,2)],
+--  [(0,3),(1,3)]]
+cols :: Matrix a -> [[Coordinates]]
+cols g = do
+  let ((x1, y1), (x2, y2)) = A.bounds g
+  [[(i, j) | i <- A.range (x1, x2)] | j <- A.range (y1, y2)]
