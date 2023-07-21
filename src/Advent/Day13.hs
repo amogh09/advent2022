b1ac1808fadfb0d2112f40b028c78420a26886b1
Amogh Rathore <amoghdroid09@gmail.com>
2023-07-21 08:42:47 -0700

    Some refactoring

--- src/Advent/Day13.hs
+++ src/Advent/Day13.hs
@@ -13,6 +13,8 @@ solve1 =
     . zip [1 :: Int, 2 ..]
     . parsePairs
 
+type Pair = (Packet, Packet)
+
 data Packet = PInt Int | PList [Packet] deriving (Show)
 
 instance Eq Packet where
@@ -26,13 +28,20 @@ comp (PInt x) (PInt y) = compare x y
 comp (PList []) (PList []) = EQ
 comp (PList []) (PList _) = LT
 comp (PList _) (PList []) = GT
-comp (PList (x : xs)) (PList (y : ys)) =
-  case comp x y of
-    EQ -> comp (PList xs) (PList ys)
-    r -> r
+comp (PList (x : xs)) (PList (y : ys)) = case comp x y of
+  EQ -> comp (PList xs) (PList ys)
+  r -> r
 comp (PList xs) (PInt y) = comp (PList xs) (PList [PInt y])
 comp (PInt x) (PList ys) = comp (PList [PInt x]) (PList ys)
 
+parsePairs :: ByteString -> [Pair]
+parsePairs = fmap parsePair . splitAtEmptyLines
+
+parsePair :: [ByteString] -> Pair
+parsePair [p1, p2] = (parsePacket p1, parsePacket p2)
+parsePair ps = error $ "invalid input: expected 2 lines but found " <> show (length ps)
+
+-- | Type of elements in stack used for parsing packets
 data StackElem = POpen | StackPacket Packet
 
 elemPacket :: StackElem -> Packet
@@ -42,13 +51,6 @@ elemPacket _ = error "stack element is not a packet"
 isOpen :: StackElem -> Bool
 isOpen POpen = True
 isOpen _ = False
-
-parsePairs :: ByteString -> [(Packet, Packet)]
-parsePairs = fmap parsePair . splitAtEmptyLines
-
-parsePair :: [ByteString] -> (Packet, Packet)
-parsePair [p1, p2] = (parsePacket p1, parsePacket p2)
-parsePair ps = error $ "invalid input: expected 2 lines but found " <> show (length ps)
 
 parsePacket :: ByteString -> Packet
 parsePacket = elemPacket . head . f []
