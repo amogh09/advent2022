95693958f8dae942082647602f62d2c1e0f18967
Amogh Rathore <amoghdroid09@gmail.com>
2023-07-26 18:10:52 -0700

    Day 14 part 1

--- src/Advent/Solver.hs
+++ src/Advent/Solver.hs
@@ -5,6 +5,7 @@ import qualified Advent.Day10 as Day10
 import qualified Advent.Day11 as Day11
 import qualified Advent.Day12 as Day12
 import qualified Advent.Day13 as Day13
+import qualified Advent.Day14 as Day14
 import qualified Advent.Day2 as Day2
 import qualified Advent.Day3 as Day3
 import qualified Advent.Day4 as Day4
@@ -51,4 +52,5 @@ table "day12" "part1" = Day12.solve1
 table "day12" "part2" = Day12.solve2
 table "day13" "part1" = Day13.solve1
 table "day13" "part2" = Day13.solve2
+table "day14" "part1" = Day14.solve1
 table _ _ = error "bad input: day and part"
