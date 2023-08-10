af44c4463c5a2634d8833d4c18f0a128e0602b7b
Amogh Rathore <amoghdroid09@gmail.com>
2023-08-09 18:19:22 -0700

    Day 15 part 2 optimized

--- src/Advent/Day15.hs
+++ src/Advent/Day15.hs
@@ -1,10 +1,11 @@
 module Advent.Day15 (solve1, solve2) where
 
 import Advent.Util (bshow, uniq)
+import Control.Monad (liftM2)
 import Data.ByteString.Lazy.Char8 (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as B
-import Data.List (foldl', sortOn)
-import Data.Maybe (mapMaybe)
+import Data.List (foldl', sortOn, tails)
+import Data.Maybe (catMaybes, mapMaybe)
 
 {- ORMOLU_DISABLE -}
 type Coordinates = (Int, Int)
@@ -48,8 +49,45 @@ noBeaconInterval y (src@(sx, sy), beacon) = do
   if dy > d
     then Nothing
     else Just (sx - d + dy, sx + d - dy)
+
+dist :: Coordinates -> Coordinates -> Int
+dist (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
+
+yIntercepts :: SourceBeacon -> ([Int], [Int])
+yIntercepts (src@(sx, sy), beacon) = do
+  let d = dist src beacon
+      pos1 = sy - (sx - d)
+      pos2 = sy - (sx + d)
+      neg1 = sy + (sx - d)
+      neg2 = sy + (sx + d)
+  ([pos1, pos2], [neg1, neg2])
+
+liesOn :: SourceBeacon -> Coordinates -> Bool
+liesOn (s, b) c = dist s b == dist s c
+
+covers :: SourceBeacon -> Coordinates -> Bool
+covers (s, b) c = dist s b >= dist s c
+
+neighbors :: Coordinates -> [Coordinates]
+neighbors (x, y) = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+
+-- >>> intersections ((0,0), (2,0)) ((5,0), (0,0))
+-- [(1,-1),(1,1)]
+intersections :: SourceBeacon -> SourceBeacon -> [Coordinates]
+intersections sb1 sb2 = do
+  let (pos1, neg1) = yIntercepts sb1
+      (pos2, neg2) = yIntercepts sb2
+  uniq
+    . filter (liesOn sb2)
+    . filter (liesOn sb1)
+    . catMaybes
+    $ liftM2 intersection pos1 neg2 <> liftM2 intersection pos2 neg1
   where
-    dist (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
+    intersection :: Int -> Int -> Maybe Coordinates
+    intersection c1 c2 = do
+      case (c2 - c1) `divMod` 2 of
+        (q, 0) -> Just (q, q + c1)
+        _ -> Nothing
 
 solve1 :: ByteString -> ByteString
 solve1 s = do
@@ -67,22 +105,25 @@ solve1 s = do
       totalIntervalLength = sum . fmap intervalLength $ intervals
   bshow $ totalIntervalLength - yBeaconsCount
 
+pairs :: [a] -> [(a, a)]
+pairs l = [(x, y) | (x : ys) <- tails l, y <- ys]
+
+inBounds :: Int -> Int -> Coordinates -> Bool
+inBounds start end (x, y) = x >= start && x <= end && y >= start && y <= end
+
 solve2 :: ByteString -> ByteString
 solve2 s = do
   let srcBeacons = fmap parseSourceBeacon . B.lines $ s
       (start, end) = (0, 4000000)
-  bshow . freq . head . mapMaybe (f srcBeacons start end) $ [start .. end]
-  where
-    f srcBeacons start end y = do
-      let intervals =
-            takeWhile ((<= end) . fst)
-              . dropWhile ((< start) . snd)
-              . noBeaconIntervals y
-              $ srcBeacons
-      case intervals of
-        [(_, ie), (is', _)] | ie + 2 == is' -> Just (ie + 1, y)
-        [(is, _)] | is == start + 1 -> Just (start, y)
-        [(_, ie)] | ie == end - 1 -> Just (end, y)
-        _ -> Nothing
+  bshow
+    . freq
+    . head
+    . filter (inBounds start end)
+    . filter (\c -> not (any (`covers` c) srcBeacons))
+    . concatMap neighbors
+    . concatMap (uncurry intersections)
+    . pairs
+    $ srcBeacons
 
-    freq (x, y) = x * 4000000 + y
+freq :: Num a => (a, a) -> a
+freq (x, y) = x * 4000000 + y
