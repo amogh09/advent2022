a6b647a7b7edb6bbf6f21310e547ebf58001bf92
Amogh Rathore <amoghdroid09@gmail.com>
2023-10-10 08:25:21 -0700

    Add sha256 to nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,7 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/af8cd5ded7735ca1df1a1174864daab75feeb64a.tar.gz") {}
+{ pkgs ? import (fetchTarball {
+    url =  "https://github.com/NixOS/nixpkgs/archive/af8cd5ded7735ca1df1a1174864daab75feeb64a.tar.gz";
+    sha256 = "sha256:0a81s3n25l2rk86gp2yxpnxv8a33cknml8z2snfzcsjp0g1a0xr3";
+  }) {}
 }:
 pkgs.mkShell {
   nativeBuildInputs = [
